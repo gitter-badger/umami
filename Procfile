af58dafe0f692f622f72faf3ba2f527b6e2a770b
Mark Rogaski <mrogaski@pobox.com>
2018-02-26 01:42:48 -0500

    Added basic settings and Heroku procfile.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: waitress-serve --port=$PORT umami.wsgi:application
--- umami/settings.py
+++ umami/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+import dj_database_url
+from environs import Env
+
+
+env = Env()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,10 +26,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1k@+9lfklvypt2+-%m5bttu9fa0wea3oeo!5mlwp$v+1097y3u'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -74,10 +80,7 @@ WSGI_APPLICATION = 'umami.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default='sqlite:///test.db', conn_max_age=600)
 }
 
 
