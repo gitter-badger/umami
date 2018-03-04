9883e2826b8cf0a42874d4f92ee5db87c5d06ff3
Mark Rogaski <mrogaski@pobox.com>
2018-03-04 17:34:17 -0500

    Added Django AllAuth (#1).

--- umami/settings.py
+++ umami/settings.py
@@ -16,41 +16,64 @@ from django.utils.crypto import get_random_string
 import dj_database_url
 from environs import Env
 
-
+#
+#  Pull all configuration from the environment
+#
 env = Env()
+try:
+    env.read_env()
+except FileNotFoundError:
+    pass
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY', get_random_string(50, 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'))
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', False)
-INTERNAL_IPS = env.list('INTERNAL_IPS', [])
-
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', [])
-
-
-# Application definition
-
+#
+# Application loading
+#
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.sites',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
+    'debug_toolbar',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.discord',
 ]
 
+#
+# Execution
+#
+WSGI_APPLICATION = 'umami.wsgi.application'
+SECRET_KEY = env('SECRET_KEY', get_random_string(50, 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'))
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', [])
+SITE_ID = 1
+TIME_ZONE = 'UTC'
+USE_TZ = True
+
+#
+# Development
+#
+DEBUG = env.bool('DEBUG', False)
+
+#
+# Security
+#
+INTERNAL_IPS = env.list('INTERNAL_IPS', [])
+
+#
+# Routing
+#
+ROOT_URLCONF = 'umami.urls'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -59,37 +82,37 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'umami.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'umami.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
+#
+# Services
+#
 DATABASES = {
     'default': dj_database_url.config(default='sqlite:///test.db', conn_max_age=600)
 }
+EMAIL_HOST = env('EMAIL_HOST', 'localhost')
+EMAIL_PORT = env.int('EMAIL_PORT', 25)
+EMAIL_HOST_USER = env('EMAIL_HOST_USER', None)
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', None)
+EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', True)
+EMAIL_TIMEOUT = env.int('EMAIL_TIMEOUT', None)
+SERVER_EMAIL = env('SERVER_EMAIL', 'root@localhost')
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL', 'webmaster@localhost')
 
+#
+# Authentication and authorization
+#
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
 
-# Password validation
-# https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
+LOGIN_REDIRECT_URL = '/'
+
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+SOCIALACCOUNT_EMAIL_REQUIRED = True
+SOCIALACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+SOCIALACCOUNT_QUERY_EMAIL = True
+SOCIALACCOUNT_STORE_TOKENS = False
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -106,23 +129,70 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+#
 # Internationalization
-# https://docs.djangoproject.com/en/2.0/topics/i18n/
-
+#
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
 
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+#
+# Static content
+#
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+#
+# Templates
+#
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+#
+# Logging
+#
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            'format': '%(asctime)s %(name)s %(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'console'
+        },
+        'null': {
+            'class': 'logging.NullHandler',
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': env('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+        'django.template': {
+            'handlers': ['null'],
+            'propagate': False
+        }
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': env('DJANGO_LOG_LEVEL', 'INFO'),
+    }
+}
